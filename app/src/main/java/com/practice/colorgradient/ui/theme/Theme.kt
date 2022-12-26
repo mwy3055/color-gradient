817a6698314800c11ef8efb39bcb9b1754564de1
Wooyoung Myung <mwy3055@gmail.com>
2022-12-26 16:06:30 +0900

    feature: color scheme 정의
    스키마는 Figma의 Color Gradients 파일을 참고했다.

--- app/src/main/java/com/practice/colorgradient/ui/theme/Theme.kt
+++ app/src/main/java/com/practice/colorgradient/ui/theme/Theme.kt
@@ -2,6 +2,7 @@ package com.practice.colorgradient.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -10,31 +11,65 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 
+@VisibleForTesting
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Primary80,
+    onPrimary = Primary20,
+    primaryContainer = Primary30,
+    onPrimaryContainer = Primary90,
+    secondary = Secondary80,
+    onSecondary = Secondary20,
+    secondaryContainer = Secondary30,
+    onSecondaryContainer = Secondary90,
+    tertiary = Tertiary80,
+    onTertiary = Tertiary20,
+    tertiaryContainer = Tertiary30,
+    onTertiaryContainer = Tertiary90,
+    error = Error80,
+    onError = Error20,
+    errorContainer = Error30,
+    onErrorContainer = Error90,
+    background = Neutral99,
+    onBackground = Neutral10,
+    surface = Neutral99,
+    onSurface = Neutral10,
+    surfaceVariant = NeutralVariant30,
+    onSurfaceVariant = NeutralVariant80,
+    outline = NeutralVariant60,
 )
 
+@VisibleForTesting
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Primary40,
+    onPrimary = Primary100,
+    primaryContainer = Primary90,
+    onPrimaryContainer = Primary10,
+    secondary = Secondary40,
+    onSecondary = Secondary100,
+    secondaryContainer = Secondary90,
+    onSecondaryContainer = Secondary10,
+    tertiary = Tertiary40,
+    onTertiary = Tertiary100,
+    tertiaryContainer = Tertiary90,
+    onTertiaryContainer = Tertiary10,
+    error = Error40,
+    onError = Error100,
+    errorContainer = Error90,
+    onErrorContainer = Error10,
+    background = Neutral99,
+    onBackground = Neutral10,
+    surface = Neutral99,
+    onSurface = Neutral10,
+    surfaceVariant = NeutralVariant90,
+    onSurfaceVariant = NeutralVariant30,
+    outline = NeutralVariant50,
 )
 
 @Composable
