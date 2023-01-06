f076b07a3813a26443204b998c04e54b5382c512
Wooyoung Myung <mwy3055@gmail.com>
2023-01-06 17:11:36 +0900

    feature: Compose Offset을 회전 변환하는 코드 작성

---
+++ app/src/main/java/com/practice/colorgradient/util/Offset.kt
@@ -0,0 +1,16 @@
+package com.practice.colorgradient.util
+
+import androidx.compose.ui.geometry.Offset
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
+/**
+ * Rotates this offset by [degree] to counter-clockwise orientation.
+ */
+fun Offset.rotate(degree: Double): Offset {
+    val theta = 2 * PI * degree / 360
+    val newX = x * cos(theta) - y * sin(theta)
+    val newY = x * sin(theta) + y * cos(theta)
+    return Offset(newX.toFloat(), newY.toFloat())
+}
---
+++ app/src/test/java/com/practice/colorgradient/OffsetTest.kt
@@ -0,0 +1,30 @@
+package com.practice.colorgradient
+
+import androidx.compose.ui.geometry.Offset
+import com.practice.colorgradient.util.rotate
+import org.junit.jupiter.api.Test
+import kotlin.math.PI
+import kotlin.math.abs
+import kotlin.math.sin
+
+class OffsetTest {
+
+    @Test
+    fun rotateTest() {
+        val offset = Offset(1f, 0f)
+        val sin = sin(PI / 4).toFloat()
+        assertEquals(Offset(0f, 1f), offset.rotate(90.0))
+        assertEquals(Offset(-1f, 0f), offset.rotate(180.0))
+        assertEquals(Offset(0f, -1f), offset.rotate(270.0))
+        assertEquals(offset, offset.rotate(360.0))
+        assertEquals(Offset(sin, sin), offset.rotate(45.0))
+    }
+
+    private fun assertEquals(expected: Offset, actual: Offset) {
+        assert(expected.x.similar(actual.x) && expected.y.similar(actual.y)) {
+            "Expected: $expected\n  Actual: $actual"
+        }
+    }
+
+    private fun Float.similar(other: Float) = abs(this - other) <= 1e9
+}
