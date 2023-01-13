2f0a827c8b45127e3c799cc6d34c118d874da829
Wooyoung Myung <mwy3055@gmail.com>
2023-01-13 22:35:35 +0900

    feature: dynamicColor의 기본값을 false로 설정
    Dynamic Color를 사용하지 않기로 결정한다. 배경화면의 색보다 내 앱의 색을 전달하고 싶다.

--- app/src/main/java/com/practice/colorgradient/ui/theme/Theme.kt
+++ app/src/main/java/com/practice/colorgradient/ui/theme/Theme.kt
@@ -11,11 +11,9 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 
 @VisibleForTesting
@@ -76,7 +74,7 @@ private val LightColorScheme = lightColorScheme(
 fun ColorGradientTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
