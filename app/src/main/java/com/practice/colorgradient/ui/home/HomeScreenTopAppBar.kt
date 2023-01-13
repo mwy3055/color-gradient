389b89b320a685d8514b8f56374c38b6c80acb3a
Wooyoung Myung <mwy3055@gmail.com>
2023-01-13 22:36:45 +0900

    feature: modifier를 모두 바깥으로 옮기고, textColor 매개변수 추가
    공간은 차지하지만 텍스트는 보이지 않게 설정하기 위한 초석이다.

--- app/src/main/java/com/practice/colorgradient/ui/home/HomeScreenTopAppBar.kt
+++ app/src/main/java/com/practice/colorgradient/ui/home/HomeScreenTopAppBar.kt
@@ -1,7 +1,5 @@
 package com.practice.colorgradient.ui.home
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -9,7 +7,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,22 +15,17 @@ import com.practice.colorgradient.ui.theme.ColorGradientTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreenTopAppBar(modifier: Modifier = Modifier) {
+fun HomeScreenTopAppBar(
+    modifier: Modifier = Modifier,
+    textColor: Color = MaterialTheme.colorScheme.onSurface,
+) {
     CenterAlignedTopAppBar(
-        modifier = modifier
-            .background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(
-                        MaterialTheme.colorScheme.primaryContainer,
-                        MaterialTheme.colorScheme.surface,
-                    ),
-                ),
-            )
-            .fillMaxWidth(),
+        modifier = modifier,
         title = {
             Text(
                 text = stringResource(R.string.app_name),
                 style = MaterialTheme.typography.titleLarge,
+                color = textColor,
             )
         },
         colors = TopAppBarDefaults.centerAlignedTopAppBarColors(containerColor = Color.Transparent),
